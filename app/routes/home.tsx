669e6b38bd4686a09f347482e19dca3c708edbb4
qninhdt <qndt123@gmail.com>
2025-05-14 18:42:58 +0700

    fix bug

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -222,7 +222,7 @@ export default function HomePage() {
         ) : (
           <div className="space-y-3">
             {folders.slice(0, 3).map((folder) => (
-              <Link to={`/edit-folder/${folder.id}`} key={folder.id}>
+              <Link to={`/folder/${folder.id}`} key={folder.id}>
                 <div className="bg-[#252525] rounded-xl p-4 mb-2">
                   <div className="flex justify-between items-center">
                     <div>
